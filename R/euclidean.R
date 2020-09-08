5ffe1e79f5ce0f0a0cb369ab72508cd73c1e41d9
Alejo Perez Gomez <alejo.perez@estudiant.upc.edu>
2020-09-08 12:42:15 +0200

    DOCS

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,7 +1,5 @@
 #' Finds the greatest common divisor of two numbers
-#'
-#' \code{euclidean}
-#' @seealso Find Wikipedia docs \href{https://en.wikipedia.org/wiki/Euclidean}{Here}
+#' Find Wikipedia docs \href{https://en.wikipedia.org/wiki/Euclidean}{Here}
 #' @param value1 A number.
 #' @param value2 A number.
 #' @return  Greatest common divisor.
