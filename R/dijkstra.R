ba0d1893e2e392d918a02f7799a965291ee32a85
Alejo Perez Gomez <alejo.perez@estudiant.upc.edu>
2020-09-08 16:36:39 +0200

    Code doc of Dijkstra

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -1,9 +1,6 @@
 #' Dijksta's algorithm 
 #'
 #'\code{dijkstra} finds shortest distance to other nodes
-#'
-#'\url{http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}  // not working!!!!!
-#'
 #' @param graph graph with 3 rows of nodes and edges.
 #' @param init_node initial node (scalar).
 #' @return  vector of distances
@@ -14,6 +11,8 @@
 #'           w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
 #' dijkstra(wiki_graph, 1)
 #' dijkstra(wiki_graph, 3)
+#' @description 
+#' Euclidean function, finds the greatest common divisor of two numbers. Find Wikipedia docs \href{http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}{Here}.
 #' @export
 
 
