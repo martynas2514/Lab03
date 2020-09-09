2510231084345f8f6603166dea8316ab2cac4529
Alejo Perez Gomez <alejo.perez@estudiant.upc.edu>
2020-09-09 12:40:41 +0200

    Upload Package Documentation

---
+++ R/lab03.R
@@ -0,0 +1,12 @@
+#' lab03: A package intended for the R package learning and Statistic computation
+#'
+#' The Lab03 package provides 2 important functions:
+#' Euclidean Distance and Dijkstra's Algorithm.
+#' 
+#' @section lab03 functions:
+#' The lab functions are named dijkstra and euclidean
+#'
+#' @docType package
+#' @name lab03
+NULL
+#> NULL
