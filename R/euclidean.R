33b25a97d665fc22965393290ef6873ab119ae46
Alejo Perez Gomez <alejo.perez@estudiant.upc.edu>
2020-09-09 12:20:33 +0200

    Line comments

--- R/euclidean.R
+++ R/euclidean.R
@@ -13,20 +13,26 @@
 
 euclidean <- function(value1, value2){
   
+  #Assert that the input is in the correct type (numeric, integer)
   stopifnot(is.numeric(value1) && length(value1) == 1) 
   stopifnot(is.numeric(value2) && length(value2) == 1)
+  
+  #Convert inputs to the the absolute value 
   value1 <- abs(value1)
   value2 <- abs(value2)
+  
+  #Case with 2 zeros in the input
   if (value1==0 && value2==0){
     value1 <- value1
   }
+  #Cases with 1 zero in the input
   else if (value1 == 0 && value2 != 0){
     value1 <- value2
   }
   else if (value1 != 0 && value2 == 0){
     value1 <- value1
   }
-  
+  #Case 2 inputs different to zero
   else {while(value1 != value2){
     if(value1 > value2){
       value1 <- value1 - value2
